274ae071245fcbaa58afb21c3d886b802e333c8f
jenchieh <jcs090218@gmail.com>
2017-09-23 06:06:17 +0800

    open todo file with notepad

---
+++ src/jcs_open_todo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ========================================================================
+# $File: jcs_open_todo.sh $
+# $Date: 2017-09-23 05:52:45 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright (c) 2017 by Shen, Jen-Chieh $
+# ========================================================================
+
+# ------------------------------------------------------------------------
+# Show the todo file.
+# ------------------------------------------------------------------------
+# Set the todo file path here..
+TODO_FILE_PATH=
+# ------------------------------------------------------------------------
+
+notepad "$TODO_FILE_PATH"
