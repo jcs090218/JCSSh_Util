a203c341839f8ca8910ed86d48c8ccc56a53ecff
jenchieh <jcs090218@gmail.com>
2017-09-23 06:06:32 +0800

    show todo file in terminal.

---
+++ src/jcs_show_todo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ========================================================================
+# $File: jcs_show_todo.sh $
+# $Date: 2017-09-23 06:00:47 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright (c) 2017 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+# ------------------------------------------------------------------------
+# Show the todo file.
+# ------------------------------------------------------------------------
+# Set the todo file path here..
+TODO_FILE_PATH=
+# ------------------------------------------------------------------------
+
+cat "$TODO_FILE_PATH"
