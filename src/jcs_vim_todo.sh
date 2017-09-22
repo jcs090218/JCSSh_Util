166cb678c346096680b8909c916f02824ff8e034
jenchieh <jcs090218@gmail.com>
2017-09-23 06:05:52 +0800

    open todo file with vim

---
+++ src/jcs_vim_todo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ========================================================================
+# $File: jcs_vim_todo.sh $
+# $Date: 2017-09-23 06:03:23 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright (c) 2017 by Shen, Jen-Chieh $
+# ========================================================================
+
+# ------------------------------------------------------------------------
+# Open the todo file in Vim.
+# ------------------------------------------------------------------------
+# Set the todo file path here..
+TODO_FILE_PATH=
+# ------------------------------------------------------------------------
+
+vim "$TODO_FILE_PATH"
