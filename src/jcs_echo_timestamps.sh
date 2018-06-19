ade84a04a79a9b7dbf530531f7ed0ba69241c81b
jenchieh <jcs090218@gmail.com>
2018-06-19 21:14:33 +0800

    Add timestamp module.

---
+++ src/jcs_echo_timestamps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ========================================================================
+# $File: jcs_echo_timestamps.sh $
+# $Date: 2018-06-19 20:45:15 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2018 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+echo
+printf "Ver. 1 " && date +"%Y-%m-%d %H:%M:%S"
+printf "Ver. 2 " && date +"%Y/%m/%d %H:%M:%S"
+echo
