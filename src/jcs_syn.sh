9b8be2eb031c8a7da1d57c6615264ea835c8982d
jenchieh <jcs090218@gmail.com>
2019-03-22 22:15:44 +0800

    Add syn.

---
+++ src/jcs_syn.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ========================================================================
+# $File: jcs_syn.sh $
+# $Date: 2019-03-22 22:11:12 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+# DESCRIPTION(jenchieh): Open dictionary in the browser by passing the word
+# you want to search for synonyms.
+
+if [ -z "$1" ]
+then
+    echo
+    echo "  "-- Dictionary Usage (Synonyms) --
+    echo
+    echo "  "Type the word you want to search in dictionary.
+    echo
+    echo "    "jcs_syn { %word }
+    echo
+    echo
+else
+    gnome-terminal -e "sensible-browser https://www.collinsdictionary.com/dictionary/english-thesaurus/$1"
+fi
